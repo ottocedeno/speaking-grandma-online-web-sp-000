73d5635710f537f5605f9d64eeac481cd75f8719
Otto Cedeno <otto@ottostacos.com>
2019-11-05 23:00:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
